85beede65f818cb34f4984d7ab8b11754a15834c
Garvit Singhal <garvitsinghal305@gmail.com>
2025-08-15 15:15:23 +0530

    Created Mongoose Connection

--- server/index.js
+++ server/index.js
@@ -1,10 +1,25 @@
-import express from "express";
+//configure dotenv to access env file
+import dotenv from 'dotenv';
+dotenv.config();                    
+
+import express from "express";      
 const app = express();
+
+import bodyParser from 'body-parser';           //body-parser is used to streamline input and parsing the raw data coming from input and manipulate req.body
+import cors from 'cors'                         //Must - used to access cross origin request access
+import connectDB from './database/index.js'
+connectDB();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}))
+app.use(cors());
 
 app.get('/',(req,res)=>{
     res.send("Hello World");
 })
 
-app.listen(3000,()=>{
+const port = process.env.PORT;
+
+app.listen(port,()=>{
     console.log("Server Started");
 });

566364c9c94a2c3d503f23baf841ae4b923d67ee
Garvit Singhal <garvitsinghal305@gmail.com>
2025-08-15 15:44:16 +0530

    Created User Schema in Mongoose

---
+++ server/database/models/user.schema.js
@@ -0,0 +1,35 @@
+import mongoose, { mongo } from 'mongoose'
+const {Schema} = mongoose;
+
+const UserSchema = new Schema({
+  _id: {
+    type: Schema.Types.ObjectId,
+    required: true,
+  },
+  name: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  role: {
+    type: String,
+    required: true,
+    default: "patient",
+    enum: ["patient", "admin", "doctor"],
+  },
+  doctors: [
+    {
+      type: Schema.Types.ObjectId,
+    },
+  ]
+})
+
+const User = mongoose.model("User",UserSchema);
+export default User;

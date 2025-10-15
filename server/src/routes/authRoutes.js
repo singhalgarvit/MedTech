5a7e9d93ee31b6e83ee107e580befd5303512965
Garvit Singhal <garvitsinghal305@gmail.com>
2025-10-15 22:53:16 +0530

    Fix:fixed the zod schema for login

--- server/src/routes/authRoutes.js
+++ server/src/routes/authRoutes.js
@@ -3,9 +3,9 @@ const router = express.Router();
 import authController from '../controllers/authController.js'
 import userExist from '../../middleware/userExist.js'
 import {validate} from '../../middleware/validate.js'
-import { userSchema } from '../../validators/userSchema.js'
+import { loginSchema, userSchema } from '../../validators/userSchema.js'
 
-router.post('/login',validate(userSchema),userExist(true),authController.login)
+router.post('/login',validate(loginSchema),userExist(true),authController.login)
 router.post('/signup',validate(userSchema),userExist(false),authController.signup)
 
 export default router;
--- server/validators/userSchema.js
+++ server/validators/userSchema.js
@@ -37,3 +37,8 @@ export const userSchema = z.object({
     )
     .optional(),
 });
+
+export const loginSchema = z.object({
+  email: z.string().email("Invalid email address"),
+  password: z.string().min(6, "Password must be at least 6 characters long"),
+});

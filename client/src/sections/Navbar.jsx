29f6057040572c714fcc6dd531aa6a3fffc85023
Garvit Singhal <garvitsinghal305@gmail.com>
2025-11-03 22:14:34 +0530

    Navbar redesign : added icons

--- client/src/sections/Navbar.jsx
+++ client/src/sections/Navbar.jsx
@@ -1,42 +1,70 @@
 import {useContext, useState} from "react";
 import LogoutButton from "../components/LogoutButton";
-import {Link} from "react-router-dom";
+import {Link, NavLink} from "react-router-dom";
 import {AuthContext} from "../context/authContext";
 import {getFullName} from "../utils/getName";
 import {CgProfile} from "react-icons/cg";
 import Button from "../components/Button";
 import {TiThMenuOutline} from "react-icons/ti";
+import {BiHomeSmile} from "react-icons/bi";
+import {FiUsers} from "react-icons/fi";
+import {RiUserSettingsLine, RiContactsBook2Line} from "react-icons/ri";
 
 function Navbar() {
   const {token} = useContext(AuthContext);
   let FullName;
-  if(!token){
+  if (!token) {
     FullName = "";
-  }else{
+  } else {
     FullName = getFullName(token);
   }
   const [open, setOpen] = useState(false);
 
   const data = [
-    {name: "Home", link: "/"},
-    {name: "Our Doctors", link: "/doctors"},
-    {name: "Services", link: "/services"},
-    {name: "Contact", link: "/contact"},
-    {name: token?FullName:"Login", link:token? "/dashboard":"/login", icon:<CgProfile className="inline-block" size={25} />}
-  ];  
+    {
+      name: "Home",
+      link: "/",
+      icon: <BiHomeSmile className="inline-block" size={25} />,
+    },
+    {
+      name: "Our Doctors",
+      link: "/doctors",
+      icon: <FiUsers className="inline-block" size={22} />,
+    },
+    {
+      name: "Services",
+      link: "/services",
+      icon: <RiUserSettingsLine className="inline-block" size={22} />,
+    },
+    {
+      name: "Contact",
+      link: "/contact",
+      icon: <RiContactsBook2Line className="inline-block" size={25} />,
+    },
+    {
+      name: token ? FullName : "Login",
+      link: token ? "/dashboard" : "/login",
+      icon: <CgProfile className="inline-block" size={25} />,
+    },
+  ];
 
   return (
     <nav className="sticky top-0 flex justify-between items-center pl-2 py-2 md:p-4 bg-blue-800 text-white z-10">
       <h1 className="text-2xl font-bold">MedTech</h1>
-      <div className="hidden md:flex space-x-6">
+      <div className="hidden md:flex gap-6">
         {data.map((item, index) => (
-          <Link
+          <NavLink
             to={item.link}
             key={index}
-            className="hover:bg-blue-950 p-3 rounded transition-all"
+            className={({isActive}) =>
+              `flex flex-row items-center gap-1  p-3 rounded transition-all duration-300 ${
+                isActive ? "bg-gray-400 text-black" : "hover:bg-blue-950"
+              }`
+            }
           >
-            {item.icon} {item.name}
-          </Link>
+            <div>{item.icon}</div>
+            <div>{item.name}</div>
+          </NavLink>
         ))}
         {token && <LogoutButton />}
       </div>
@@ -54,9 +82,10 @@ function Navbar() {
               <Link
                 to={item.link}
                 key={index}
-                className="block px-4 py-2 hover:bg-gray-200"
+                className="flex flex-row items-center gap-1 px-4 py-2 hover:bg-gray-200"
               >
-                {item.icon} {item.name}
+                <div>{item.icon}</div>
+                <div>{item.name}</div>
               </Link>
             ))}
             {token && <LogoutButton className="block w-full px-4 py-2 mt-2" />}
